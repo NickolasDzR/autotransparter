e851b75ac9be7a2572a08b869b91e65529fce255
NickolasDzR <nikolyae2019@gmail.com>
2021-02-27 22:07:29 +0300

    справил слайдер

--- src/blocks/modules/examples/examples.js
+++ src/blocks/modules/examples/examples.js
@@ -1,4 +1,37 @@
 import Glide from "@glidejs/glide";
+import { siblings } from "@glidejs/glide/src/utils/dom";
+
+const CustomActiveClass = function (Glide, Components, Events) {
+    var Component = {
+        mount() {
+            this.changeActiveSlide();
+        },
+
+        changeActiveSlide() {
+            let slide = Components.Html.slides[Glide.index];
+            slide.classList.remove("is-next", "is-prev");
+            slide.classList.add("is-active");
+
+            siblings(slide).forEach((sibling) => {
+                sibling.classList.remove("is-active", "is-next", "is-prev");
+            });
+
+            if(slide.nextElementSibling) {
+                slide.nextElementSibling.classList.add("is-next");
+            }
+
+            if(slide.previousElementSibling) {
+                slide.previousElementSibling.classList.add("is-prev");
+            }
+        },
+    };
+
+    Events.on("run", () => {
+        Component.changeActiveSlide();
+    });
+
+    return Component;
+};
 
 new Glide(".glide", {
     type: "carousel",
@@ -6,11 +39,13 @@ new Glide(".glide", {
     startAt: 3,
     perView: 2.2,
     focusAt: "center",
-    animationDuration: 150,
+    animationDuration: 500,
     rewindDuration: 0,
     breakpoints: {
         768: {
             perView: 1.4
         }
     }
-}).mount();
+}).mount({
+    "CustomActiveClass": CustomActiveClass,
+});
